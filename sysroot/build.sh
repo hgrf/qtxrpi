4500f4f86e8f994b50a981993eb31b8ff4d5d1df
Holger Graef <holger.graef@gmail.com>
2023-08-05 23:06:16 +0200

    fix rsync paths

--- sysroot/build.sh
+++ sysroot/build.sh
@@ -2,6 +2,6 @@
 cd /sysroot
 rm -rf lib usr/lib usr/include
 mkdir -p lib usr/lib usr/include
-rsync -avh /lib lib
-rsync -avh /usr/lib usr/lib
-rsync -avh /usr/include usr/include
+rsync -avh /lib ./
+rsync -avh /usr/lib usr/
+rsync -avh /usr/include usr/
